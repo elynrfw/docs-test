ce9033a0caa7e2072e367e91af5b56cff21e8233
Elyn Fritz-Waters <61986313+elynrfw@users.noreply.github.com>
2024-08-29 15:02:54 -0500

    Update conf.py

--- conf.py
+++ conf.py
@@ -57,7 +57,10 @@ exclude_patterns = ["_build", "Thumbs.db", ".DS_Store"]
 # a list of builtin themes.
 #
 #html_theme = "sphinx_rtd_theme"
-html_theme = "piccolo_theme"
+#html_theme = "piccolo_theme"
+html_theme = "press"
+#html_theme = "insegel"
+#html_theme = "sizzle"
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
